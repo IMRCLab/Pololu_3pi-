742a866c5ca92c97fa255e0dd695b7fc3d9cc274
Jan Achermann <achermann@campus.tu-berlin.de>
2024-11-05 16:02:14 +0100

    decode messages sceletton

--- uasync_uart.py
+++ uasync_uart.py
@@ -1,40 +1,61 @@
 import struct
 from machine import Timer, UART, Pin
 import uasyncio as asyncio
-import micropython, copy
+import micropython
 
 tsf = asyncio.ThreadSafeFlag()
 buffer= bytearray(16) #TODO change chack tp 32
 pin = Pin(29)
-uart0 = UART(0, baudrate=9600, tx=Pin(28), rx=Pin(29), bits=8, parity=None, stop=1,rxbuf=1000)
-flag = False
+uart0 = UART(0, baudrate=115200, tx=Pin(28), rx=Pin(29), bits=8, parity=None, stop=1,rxbuf=1000)
+flag_received = False
+flag_decoded = False
 
 
 async def read_uart():
     global buffer
-    global flag
+    global flag_received
+    global uart0
+    await asyncio.sleep(1)
     while True:
         await asyncio.sleep(0)
         if uart0.any():
             uart0.readinto(buffer)
-            flag = True
+            flag_received = True
 
-        pass
-
-async def main():
-    global flag
+async def decode_message():
     global buffer
-    asyncio.run(read_uart())
+    global flag_received
+    global flag_decoded
+    await asyncio.sleep(1)
     while True:
         await asyncio.sleep(0)
-        if flag:
-            #TODO deinint irq before reading and reinit after 
+        if flag_received:
+            if buffer[0] == 1:
+                print("1")
+                pass
+            elif buffer[0] == 2:
+                print("2")
+                pass
+            else :
+                print("else")
+                pass
+
+            flag_received = False
+            flag_decoded = True
+
+async def main():
+    print("start")
+    global flag_received
+    global flag_decoded
+    global buffer
+    task1 = asyncio.create_task(read_uart())
+    task2 = asyncio.create_task(decode_message())
+    print("main")
+    while True:
+        await asyncio.sleep(0)
+        if flag_decoded:
             print(buffer)
-            value1 = struct.unpack('<f',buffer[-4:])[0]
-            value2 = struct.unpack('<f',buffer[-9:-5])[0]
-            value3 = struct.unpack('<f',buffer[-13:-9])[0]
-            print(f"value3:{value3} : {buffer[-13:-9]}, value2:{value2} ; {buffer[-9:-5]},value1:{value1} : {buffer[-4:]}")
-            flag = False
+            flag_decoded = False
 
 
 
