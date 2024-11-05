7b3dca46e308c33f848c9dc68769334a484f199e
Jan Achermann <achermann@campus.tu-berlin.de>
2024-11-05 18:01:06 +0100

    uart async test changes

--- uasync_uart.py
+++ uasync_uart.py
@@ -5,6 +5,7 @@ import micropython
 
 tsf = asyncio.ThreadSafeFlag()
 buffer= bytearray(16) #TODO change chack tp 32
+buffer_list = list()
 pin = Pin(29)
 uart0 = UART(0, baudrate=115200, tx=Pin(28), rx=Pin(29), bits=8, parity=None, stop=1,rxbuf=1000)
 flag_received = False
@@ -12,15 +13,17 @@ flag_decoded = False
 
 
 async def read_uart():
-    global buffer
+    global buffer_list
     global flag_received
     global uart0
     await asyncio.sleep(1)
     while True:
         await asyncio.sleep(0)
         if uart0.any():
-            uart0.readinto(buffer)
-            flag_received = True
+            new_buffer = bytearray(32)
+            uart0.readinto(new_buffer)
+            buffer_list.append(new_buffer)
+            #flag_received = True
 
 async def decode_message():
     global buffer
@@ -47,15 +50,19 @@ async def main():
     print("start")
     global flag_received
     global flag_decoded
-    global buffer
+    global buffer_list
     task1 = asyncio.create_task(read_uart())
-    task2 = asyncio.create_task(decode_message())
+    #task2 = asyncio.create_task(decode_message())
     print("main")
     while True:
         await asyncio.sleep(0)
-        if flag_decoded:
+        try:
+            print(buffer_list.pop(0))
+        except:
+            pass
+        """if flag_decoded:
             print(buffer)
-            flag_decoded = False
+            flag_decoded = False"""
 
 
 
