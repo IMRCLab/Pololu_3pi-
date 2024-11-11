d0a7b2305351f4bffdf95ab1d5ddd5dca8f81d9e
Jan Achermann <achermann@campus.tu-berlin.de>
2024-11-11 18:17:41 +0100

    uart message read update

--- uart.py
+++ uart.py
@@ -2,12 +2,14 @@ import struct
 from machine import Timer, UART, Pin
 import uasyncio as asyncio
 import micropython
+from primitives.queue import Queue, QueueEmpty
+from quaternion import Quaternion
 
 class Uart():
     def __init__(self, baudrate:int = 115200,txPin:int =28, rxPin:int = 29,bits:int=8, parity=None, stop:int=1,rxbuf:int=1000):
         self.uart =  UART(0,baudrate = baudrate, tx=Pin(txPin),rx=Pin(rxPin),bits = bits, parity= parity,stop=stop,rxbuf=rxbuf)
-        self.list_receive = list()
-        self.list_decode = list()
+        self.list_receive = Queue()
+        self.list_decode = Queue()
         self.read = asyncio.create_task(self.read_uart)
         self.decode = asyncio.create_task(self.decode_uart)
     
@@ -18,17 +20,20 @@ class Uart():
             if self.uart.any():
                 new_buffer = bytearray(32)
                 self.uart.readinto(new_buffer)
-                self.list_receive.append(new_buffer) 
+                self.list_receive.put_nowait(new_buffer) 
 
     async def decode_uart(self):
         await asyncio.sleep(1)
         while True:
             await asyncio.sleep(0)
             try:
-                buffer = self.list_receive.pop(0)
-                if buffer[0] == 1:
-                    print("1")
-                    pass
+                buffer = self.list_receive.get_nowait()
+                if buffer[0] == 0x6d and buffer[1] == 0x09: 
+                    x = struct.unpack('<h',buffer[3:5])
+                    y = struct.unpack('<h',buffer[5:7])
+                    z = struct.unpack('<h',buffer[7:9])
+                    quaternion = Quaternion(int.from_bytes(buffer[9:], byteorder='little'))
+                    self.list_decode.put_nowait([x,y,z,quaternion])
                 elif buffer[0] == 2:
                     print("2")
                     pass
@@ -38,11 +43,11 @@ class Uart():
             except:
                 pass
                 
-    def read_position(self):
-        result = None
+    def get_position(self) : # TODO: Add return type 
+        result = bytes()
         try:
-            result = self.list_decode.pop(0)
-        except:
-            result = None
+            result = self.list_decode.get_nowait()
+        except QueueEmpty:
+            result = bytes()
     
         return result
