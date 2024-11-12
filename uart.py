21ac60e418668b7cb5cb94e6b8a288a7ca7edb84
Jan Achermann <achermann@campus.tu-berlin.de>
2024-11-12 13:52:27 +0100

    update to get with wait

--- uart.py
+++ uart.py
@@ -26,28 +26,19 @@ class Uart():
         await asyncio.sleep(1)
         while True:
             await asyncio.sleep(0)
-            try:
-                buffer = self.list_receive.get_nowait()
-                if buffer[0] == 0x6d and buffer[1] == 0x09: 
-                    x = struct.unpack('<h',buffer[3:5])
-                    y = struct.unpack('<h',buffer[5:7])
-                    z = struct.unpack('<h',buffer[7:9])
-                    quaternion = Quaternion(int.from_bytes(buffer[9:], byteorder='little'))
-                    self.list_decode.put_nowait([x,y,z,quaternion])
-                elif buffer[0] == 2:
-                    print("2")
-                    pass
-                else :
-                    print("else")
-                    pass
-            except:
+            buffer = await self.list_receive.get()
+            if buffer[0] == 0x6d and buffer[1] == 0x09: 
+                x = struct.unpack('<h',buffer[3:5])
+                y = struct.unpack('<h',buffer[5:7])
+                z = struct.unpack('<h',buffer[7:9])
+                quaternion = Quaternion(int.from_bytes(buffer[9:], byteorder='little'))
+                self.list_decode.put_nowait([x,y,z,quaternion])
+            elif buffer[0] == 2:
+                print("2")
+                pass
+            else :
+                print("else")
                 pass
                 
-    def get_position(self) : # TODO: Add return type 
-        result = bytes()
-        try:
-            result = self.list_decode.get_nowait()
-        except QueueEmpty:
-            result = bytes()
-    
-        return result
+    async def get_position(self) : # TODO: Add return type 
+        return await self.list_decode.get()
