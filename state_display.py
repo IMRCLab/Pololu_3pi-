ec826a7ab64e42ad90ccd69f0d42c1173c22b120
Jan Achermann <achermann@campus.tu-berlin.de>
2025-01-31 16:39:48 +0100

    corrections

---
+++ state_display.py
@@ -0,0 +1,72 @@
+from machine import Timer, UART, Pin
+from pololu_3pi_2040_robot import robot
+import time
+
+
+class StateDisplay():
+    def __init__(self) -> None:
+        self.display = robot.Display()
+    
+    def ready(self) -> None:
+        self.display.fill(0)
+        self.display.text("     READY", 0, 0+8)
+        self.display.text("   ______", 0, 0+8+8)
+        self.display.text("  /|_||_\`.__", 0, 8+8+8)
+        self.display.text(" (   _    _ _\ ", 0,16+8+8)
+        self.display.text(" =`-(_)--(_)-'",0,24+8+8)
+        self.display.text("-- --- --- --- -",0,32+8+8)
+        self.display.text("     READY", 0, 32+8+8+8)
+        self.display.show()
+
+    def driving(self) -> None:
+        self.display.fill(0)
+        self.display.text("    DRIVING...", 0, 0+8)
+        self.display.text("   ______", 0, 0+8+8)
+        self.display.text("  /|_||_\`.__", 0, 8+8+8)
+        self.display.text(" (   _    _ _\ ", 0,16+8+8)
+        self.display.text(" =`-(_)--(_)-'",0,24+8+8)
+        self.display.text("-- --- --- --- -",0,32+8+8)
+        self.display.text("    DRIVING...", 0, 32+8+8+8)
+        self.display.show()
+
+    def driving_continously(self) -> None:
+        def shift_left(s:str)-> str:
+            if not s:  # Handle empty strings
+                return ""
+            return s[1:] + s[0]
+        road = "-- --- --- --- -"
+        for _ in range(20):
+            self.display.fill(0)
+            self.display.text("    DRIVING...", 0, 0+8)
+            self.display.text("   ______", 0, 0+8+8)
+            self.display.text("  /|_||_\`.__", 0, 8+8+8)
+            self.display.text(" (   _    _ _\ ", 0,16+8+8)
+            self.display.text(" =`-(_)--(_)-'",0,24+8+8)
+            road = shift_left(road)
+            self.display.text(road,0,32+8+8)
+            self.display.text("    DRIVING...", 0,32+8+8+8)
+            self.display.show()
+            time.sleep(0.1)
+
+    def finish_driving(self) ->None:
+        self.display.fill(0)
+        self.display.text("----------------",0,8)
+        self.display.text("|  ______      |", 0, 0+8+8)
+        self.display.text("| /|_||_\`.__  |", 0, 8+8+8)
+        self.display.text("|(   _    _ _\ |", 0,16+8+8)
+        self.display.text("|=`-(_)--(_)-' |",0,24+8+8)
+        self.display.text("----------------",0,32+8+8)
+        self.display.text("  DONE DRIVING", 0, 32+8+8+8)
+        self.display.show()
+
+    def finish_saving(self) -> None:
+        self.display.fill(0)
+        self.display.text("----------------",0,8)
+        self.display.text("|  ______      |", 0, 0+8+8)
+        self.display.text("| /|_||_\`.__  |", 0, 8+8+8)
+        self.display.text("|(   _    _ _\ |", 0,16+8+8)
+        self.display.text("|=`-(_)--(_)-' |",0,24+8+8)
+        self.display.text("----------------",0,32+8+8)
+        self.display.text("  DONE SAVING", 0, 32+8+8+8)
+        self.display.show()
+
